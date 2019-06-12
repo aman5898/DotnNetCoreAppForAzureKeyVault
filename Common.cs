d9adaf0a8358dacedd264f95127fa88cd0ac9425
Aman Kumar <amankrsingh.2012@gmail.com>
2019-06-12 14:01:03 +0530

    Key vault working using msi and aad

---
+++ Common.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+
+namespace WebAppKeyVault
+{
+    public class Common
+    {
+
+        private IConfiguration configuration;
+
+        public Common(IConfiguration iconfig)
+        {
+            configuration = iconfig;
+        }
+
+        public string Message { get; set; }
+
+        public async Task<string> OnGetAsync()
+        {
+            Message = "Your application description page.";
+            string url = configuration.GetSection("keyForVault").Value;
+            try
+            {
+
+                //var URL = _configuration["keyForVault"];
+                /* The next four lines of code show you how to use AppAuthentication library to fetch secrets from your key vault */
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                var secret = await keyVaultClient.GetSecretAsync(url)
+                        .ConfigureAwait(false);
+                Message = secret.Value;
+            }
+            
+            catch (KeyVaultErrorException keyVaultException)
+            {
+                Message = keyVaultException.Message;
+            }
+
+            return Message;
+        }
+
+        // This method implements exponential backoff if there are 429 errors from Azure Key Vault
+        private long getWaitTime(int retryCount)
+        {
+            long waitTime = ((long)Math.Pow(2, retryCount) * 100L);
+            return waitTime;
+        }
+
+        // This method fetches a token from Azure Active Directory, which can then be provided to Azure Key Vault to authenticate
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net");
+            return accessToken;
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebAppKeyVault.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private IConfiguration configuration;
+
+        public ValuesController(IConfiguration iconfig)
+        {
+            configuration = iconfig;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+         
+            Common ob = new Common(configuration);
+            Task<string> str = ob.OnGetAsync();
+            return new string[] { str.Result };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
